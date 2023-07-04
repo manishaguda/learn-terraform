00b7c169afff02a357a3714c55fb78b694a31c06
manisha guda <manishavguda@gmail.com>
2023-07-04 14:58:28 +0530

    terraform

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -10,10 +10,10 @@ output "ami" {
   value = data.aws_ami.example
 }
 
-data "aws_instances" "foo" {
-  instances_id = "i-0e0bab17ea9e03b55"
+data "aws_instance" "foo" {
+  instance_id = "i-0e0bab17ea9e03b55"
 }
 
-output "instances" {
-  value = data.aws_instances.foo.public_ip
+output "instance" {
+  value = data.aws_instance.foo.public_ip
 }
