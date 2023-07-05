20818c5d751542bc7e87c0c68df8ebbb9948566e
manisha guda <manishavguda@gmail.com>
2023-07-05 13:20:45 +0530

    terraform

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 data "aws_ami" "centos8" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos8.id
