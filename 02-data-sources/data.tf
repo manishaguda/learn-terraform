66bb64d88d1e08596004d9c49987d5fbe3ade79a
manisha guda <manishavguda@gmail.com>
2023-07-05 13:14:34 +0530

    terraform

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "example" {
+data "aws_ami" "centos8" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
@@ -7,13 +7,15 @@ data "aws_ami" "example" {
 }
 
 output "ami" {
-  value = data.aws_ami.example
+  value = data.aws_ami.centos8
+}
+
+
+
+output "instance" {
+  value = data.aws_instance.foo.public_ip
 }
 
 data "aws_instance" "foo" {
   instance_id = "i-0e0bab17ea9e03b55"
-}
-
-output "instance" {
-  value = data.aws_instance.foo.public_ip
 }
---
+++ 03-vars/vars.tf
@@ -0,0 +1,65 @@
+# String Data Type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number Data Type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data Type
+variable "sample3" {
+  default = true
+}
+
+# Default variable type
+variable "sample4" {
+  default = 100
+}
+
+output "sample4" {
+  value = var.sample4
+}
+
+# List Variable Type
+variable "sample5" {
+  default = [
+  100,
+  "abc"
+  "xyz"
+  ]
+}
+
+output "sample5" {
+  value = var.sample5[1]
+}
+
+# Map Variable Type
+variable "sample6" {
+  default = {
+    number = 100
+    string = "xyz"
+    boolean = false
+  }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+}
+
+## Variable from tfvars
+variable "demo1" {}
+
+output "demo1" {
+  value = var.demo1
+}
+
+## Variable From Shell env variables
+variable "demo2" {
+  default = null
+}
+
+output "demo2" {
+  value = var.demo2
+}
