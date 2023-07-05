2778a1269c0bcde947c645a26db849f3b346f2c4
manisha guda <manishavguda@gmail.com>
2023-07-05 15:12:57 +0530

    terraform

--- 05-remote.state/sample.tf
+++ 05-remote.state/sample.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "web" {
 terraform {
   backend "s3" {
     bucket = "terraform-b70"
-    key    = "05-remote.state/terraform.tfstate"
+    key    = "05-remote-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
